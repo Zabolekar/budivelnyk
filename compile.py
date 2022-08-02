14f5e1c3a1d57868310d8a9f1cb3e1834c3b71ea
Zabolekar <syekirin@gmail.com>
2022-08-03 00:44:01 +0200

    primitive bf to x86_64 asm compiler

---
+++ compile.py
@@ -0,0 +1,46 @@
+"""
+Compile bf to x86_64 asm. Cell size is one byte.
+"""
+
+from sys import argv
+
+def fill_jump_tables(bf_code: str) -> tuple[dict[int, int], dict[int, int]]:
+    forward_jumps = {}
+    backward_jumps = {}
+    stack = []
+    for i, c in enumerate(bf_code):
+        if c == '[':
+            stack.append(i)
+        elif c == ']':
+            j = stack.pop()
+            forward_jumps[j] = i
+            backward_jumps[i] = j
+    return forward_jumps, backward_jumps
+
+if __name__ == "__main__":
+    input_path, output_path = argv[1:]
+    with open(input_path) as input_file:
+        bf_code = input_file.read()
+
+    forward_jumps, backward_jumps = fill_jump_tables(bf_code)
+
+    with open(output_path, 'w') as output_file:
+        write = lambda *args: print(*args, sep="\n", file=output_file)
+        write(".globl run", ".type run, @function", "run:")
+        for i, c in enumerate(bf_code):
+            if c == '+':
+                write("incb (%rdi)")
+            elif c == '-':
+                write("decb (%rdi)")
+            elif c == '>':
+                write("incq %rdi")
+            elif c == '<':
+                write("decq %rdi")
+            elif c == '[':
+                write("start:", "cmpb $0, (%rdi)", "je end")
+                # TODO: enumerate labels
+            elif c == ']':
+                write("jmp start", "end:")
+                # TODO: enumerate labels
+            # TODO: ,.
+        write("ret", '.section .note.GNU-stack,"",@progbits')
---
+++ run.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+python3.10 compile.py inc.bf inc.s
+gcc -c -Wall -fpic inc.s
+gcc -shared -o libinc.so inc.o
+python3.10 test.py
