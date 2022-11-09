a6c79fece76f81b15183f55d317ff748c3dc3e21
Zabolekar <syekirin@gmail.com>
2022-11-09 01:06:53 +0100

    add support for intel syntax, make it default

--- src/budivelnyk/__init__.py
+++ src/budivelnyk/__init__.py
@@ -2,9 +2,11 @@
 Compile bf to asm. Cell size is one byte.
 """
 
+from __future__ import annotations
+import enum
 import subprocess
+from typing import Iterator
 from platform import machine
-
 
 def fill_jump_tables(intermediate: list[str]) -> dict[int, int]:
     jumps = {}
@@ -17,6 +19,7 @@ def fill_jump_tables(intermediate: list[str]) -> dict[int, int]:
             jumps[j] = i
             jumps[i] = j
     return jumps
+
 
 def bf_to_intermediate(bf_code: str) -> list[str]:
     """
@@ -42,7 +45,8 @@ def bf_to_intermediate(bf_code: str) -> list[str]:
 
     return result
 
-def generate_x86_64(intermediate: list[str]):
+
+def generate_x86_64_att(intermediate: list[str]) -> Iterator[str]:
     jumps = fill_jump_tables(intermediate)
 
     yield ".globl run"
@@ -96,21 +100,105 @@ def generate_x86_64(intermediate: list[str]):
     yield '.section .note.GNU-stack,"",@progbits'
 
 
-def bf_to_asm(input_path: str, output_path: str):
+def generate_x86_64_intel(intermediate: list[str]) -> Iterator[str]:
+    jumps = fill_jump_tables(intermediate)
+
+    yield ".globl run"
+    yield ".intel_syntax noprefix"
+    yield ".type run, @function"
+    yield "run:"
+
+    for i, command in enumerate(intermediate):
+        n = len(command)
+
+        if command.startswith("+"):
+            if n == 1:
+                yield "inc byte ptr [rdi]"
+            else:
+                yield f"add byte ptr [rdi], {n}"
+        elif command.startswith("-"):
+            if n == 1:
+                yield "dec byte ptr [rdi]"
+            else:
+                yield f"sub byte ptr [rdi], {n}"
+        elif command.startswith(">"):
+            if n == 1:
+                yield "inc rdi"
+            else:
+                yield f"add rdi, {n}"
+        elif command.startswith("<"):
+            if n == 1:
+                yield "dec rdi"
+            else:
+                yield f"sub rdi, {n}"
+        elif command == '[':
+            suffix = f"{i}_{jumps[i]}"
+            yield f"start_{suffix}:"
+            yield "cmp byte ptr [rdi], 0"
+            yield f"je end_{suffix}"
+        elif command == ']':
+            suffix = f"{jumps[i]}_{i}"
+            yield f"jmp start_{suffix}"
+            yield f"end_{suffix}:"
+        elif command.startswith("."):
+            yield "push rdi"
+            yield "movzx rdi, byte ptr [rdi]"
+            sequence = ["call putchar", "mov rdi, rax"] * n
+            yield from sequence[:-1]
+            yield "pop rdi"
+        elif command.startswith(","):
+            yield "push rdi"
+            yield from ["call getchar"] * n
+            yield "pop rdi"
+            yield "mov byte ptr [rdi], al"
+    yield "ret"
+    yield '.section .note.GNU-stack,"",@progbits'
+
+
+class Target(enum.Enum):
+    X86_64_INTEL = enum.auto()
+    X86_64_ATT = enum.auto()
+    X86_32_INTEL = enum.auto()
+    X86_32_ATT = enum.auto()
+    # ARM
+    ARM64 = enum.auto()
+    ARM32 = enum.auto()
+    # ...
+    
+    @staticmethod
+    def suggest() -> Target:
+        if machine() == "x86_64":
+            return Target.X86_64_INTEL
+        else:
+            raise RuntimeError("unsupported or unknown architecture")
+
+    @staticmethod
+    def candidates() -> tuple[Target, ...]:
+        if machine() == "x86_64":
+            return (Target.X86_64_INTEL, Target.X86_64_ATT)
+        else:
+            raise RuntimeError("unsupported or unknown architecture")
+
+
+def intermediate_to_asm(intermediate: list[str], *, target: Target) -> Iterator[str]:
+    if target == Target.X86_64_INTEL:
+        yield from generate_x86_64_intel(intermediate)
+    elif target == Target.X86_64_ATT:
+        yield from generate_x86_64_att(intermediate)
+
+
+def bf_to_asm(input_path: str, output_path: str, *, target: Target = Target.suggest()) -> None:
     with open(input_path) as input_file:
         bf_code = input_file.read()
 
     intermediate = bf_to_intermediate(bf_code)
 
     with open(output_path, 'w') as output_file:
-        if machine() == "x86_64":
-            lines = generate_x86_64(intermediate)
-            print(*lines, sep="\n", file=output_file)
-        else:
-            raise RuntimeError("unknown architecture")
+        lines = intermediate_to_asm(intermediate, target=target)
+        print(*lines, sep="\n", file=output_file)
 
 
-def bf_to_shared(input_path: str, asm_path: str, output_path: str):
-    bf_to_asm(input_path, asm_path)
+def bf_to_shared(input_path: str, asm_path: str, output_path: str, *, target: Target = Target.suggest()) -> None:
+    bf_to_asm(input_path, asm_path, target=target)
     asm_to_shared = ["cc", "-shared", "-o", output_path, asm_path]
     subprocess.run(asm_to_shared).check_returncode()
--- tests/test_bf.py
+++ tests/test_bf.py
@@ -3,29 +3,35 @@ from os import path
 from ctypes import CDLL
 from tempfile import TemporaryDirectory
 from subprocess import run, Popen, PIPE
-from budivelnyk import bf_to_shared
+import pytest
+from budivelnyk import bf_to_shared, Target
 
+targets = Target.candidates()
 
-def test_inc():
+@pytest.mark.parametrize("target", targets)
+def test_inc(target):
     with TemporaryDirectory() as tmp:
         bf = "tests/bf/inc.bf"
         asm = path.join(tmp, "inc.s")
         library = path.join(tmp, "libinc.so")
-
-        bf_to_shared(bf, asm, library)
+        bf_to_shared(bf, asm, library, target=target)
 
         libinc = CDLL(library)
-        buffer = b"Gdkkn+\x1f`rrdlakx "
+        # We need bytes(list(...)) to prevent Python from
+        # reusing the same buffer between test runs.
+        buffer = bytes(list(b"Gdkkn+\x1f`rrdlakx "))
+        print(buffer)
         libinc.run(buffer)
         assert buffer == b"Hello, assembly!"
 
 
-def test_hello():
+@pytest.mark.parametrize("target", targets)
+def test_hello(target):
     with TemporaryDirectory() as tmp:
         bf = "tests/bf/hello.bf"
         asm = path.join(tmp, "hello.s")
         library = path.join(tmp, "libhello.so")
-        bf_to_shared(bf, asm, library)
+        bf_to_shared(bf, asm, library, target=target)
 
         call_hello = [sys.executable, "tests/py/call_hello.py", library]
         result = run(call_hello, capture_output=True)
@@ -33,12 +39,13 @@ def test_hello():
         assert result.stdout == b"hello\n"
 
 
-def test_echo():
+@pytest.mark.parametrize("target", targets)
+def test_echo(target):
     with TemporaryDirectory() as tmp:
         bf = "tests/bf/echo.bf"
         asm = path.join(tmp, "echo.s")
         library = path.join(tmp, "libecho.so")
-        bf_to_shared(bf, asm, library)
+        bf_to_shared(bf, asm, library, target=target)
 
         call_echo = [sys.executable, "tests/py/call_echo.py", library]
         with Popen(call_echo, stdin=PIPE, stdout=PIPE, stderr=PIPE) as process:
@@ -48,12 +55,13 @@ def test_echo():
             assert not error
 
 
-def test_fibs():
+@pytest.mark.parametrize("target", targets)
+def test_fibs(target):
     with TemporaryDirectory() as tmp:
         bf = "tests/bf/fibs.bf"
         asm = path.join(tmp, "fibs.s")
         library = path.join(tmp, "libfibs.so")
-        bf_to_shared(bf, asm, library)
+        bf_to_shared(bf, asm, library, target=target)
 
         libfibs = CDLL(library)
         buffer = bytes([0, 1, 0, 0])
