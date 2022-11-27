9937b340aa025030a6b7a8686a3639df1346a386
Zabolekar <syekirin@gmail.com>
2022-11-28 00:13:13 +0100

    mutating bytes objects is fragile, use ctypes buffers

--- tests/py/call_reads.py
+++ tests/py/call_reads.py
@@ -1,7 +1,7 @@
 from sys import argv
-from ctypes import CDLL
+from ctypes import CDLL, create_string_buffer
 
 libreads = CDLL(argv[1])
-buffer = bytes(1)
+buffer = create_string_buffer(1)
 libreads.run(buffer)
-assert buffer == b"c"
+assert bytes(buffer) == b"c"
