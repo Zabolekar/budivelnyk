6759c7139d5aa258dc32bb58d67e08c5bb7dc510
Zabolekar <syekirin@gmail.com>
2022-12-08 21:39:46 +0100

    move Target class to separate file

--- src/budivelnyk/__init__.py
+++ src/budivelnyk/__init__.py
@@ -2,64 +2,17 @@
 Compile bf to asm. Cell size is one byte.
 """
 
-from __future__ import annotations
-import enum
 import subprocess
-import platform
 from warnings import warn
 from typing import Iterator
 
 from .intermediate import AST, bf_to_intermediate
-from .x86_64_att import generate_x86_64_att
-from .x86_64_intel import generate_x86_64_intel
-from .arm64 import generate_arm64
-from .riscv64 import generate_riscv64
-
-class Target(enum.Enum):
-    X86_64_INTEL = enum.auto()
-    X86_64_ATT = enum.auto()
-    ARM64 = enum.auto()
-    RISCV64 = enum.auto()
-    
-    @staticmethod
-    def suggest() -> Target:
-        return Target.candidates()[0]
-
-    @staticmethod
-    def candidates() -> tuple[Target, ...]:
-        system = platform.system()
-        if system == "Linux":
-            machine = platform.machine()
-            if machine == "x86_64":
-                return (Target.X86_64_INTEL, Target.X86_64_ATT)
-            elif machine == "riscv64":
-                return (Target.RISCV64,)
-            else:
-                raise RuntimeError(f"Linux on {machine} is not supported")
-        elif system in ("NetBSD", "OpenBSD"):
-            processor = platform.processor()
-            if processor == "aarch64":
-                return (Target.ARM64,)
-            else:
-                raise RuntimeError(f"{system} on {processor} is not supported")
-        else:
-            raise RuntimeError(f"unsupported or unknown OS: {system}")
-        
-
-def intermediate_to_asm(intermediate: AST, *, target: Target) -> Iterator[str]:
-    if target == Target.X86_64_INTEL:
-        yield from generate_x86_64_intel(intermediate)
-    elif target == Target.X86_64_ATT:
-        yield from generate_x86_64_att(intermediate)
-    elif target == Target.ARM64:
-        yield from generate_arm64(intermediate)
-    elif target == Target.RISCV64:
-        yield from generate_riscv64(intermediate)
+from .target import Target
 
 
 def bf_to_asm(bf_code: str, *, target: Target = Target.suggest()) -> Iterator[str]:
     intermediate: AST = bf_to_intermediate(bf_code)
-    yield from intermediate_to_asm(intermediate, target=target)
+    yield from target.intermediate_to_asm(intermediate)
 
 
 def bf_file_to_asm_file(input_path: str, output_path: str, *, target: Target = Target.suggest()) -> None:
