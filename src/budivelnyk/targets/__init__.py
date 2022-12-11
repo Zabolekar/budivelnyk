588b9a8a7c27886ece1901683b3e5e9e4b964df8
Zabolekar <syekirin@gmail.com>
2022-12-11 22:41:13 +0100

    reordering targets alphabetically, adding arm32 boilerplate

--- src/budivelnyk/targets/__init__.py
+++ src/budivelnyk/targets/__init__.py
@@ -4,10 +4,11 @@ import platform
 from typing import Iterator
 
 from ..intermediate import AST
-from .x86_64_att import generate_x86_64_att
-from .x86_64_intel import generate_x86_64_intel
+from .arm32 import generate_arm32
 from .arm64 import generate_arm64
 from .riscv64 import generate_riscv64
+from .x86_64_att import generate_x86_64_att
+from .x86_64_intel import generate_x86_64_intel
 
 # platform.machine can be not specific enough on NetBSD,
 # platform.processor can be empty on Linux, so we have
@@ -15,10 +16,12 @@ from .riscv64 import generate_riscv64
 
 def _linux_candidates(machine: str) -> tuple[Target, ...]:
     match machine:
-        case "x86_64":
-            return (Target.X86_64_INTEL, Target.X86_64_ATT)
+        case "armv7l":
+            return (Target.ARM32,)
         case "riscv64":
             return (Target.RISCV64,)
+        case "x86_64":
+            return (Target.X86_64_INTEL, Target.X86_64_ATT)
         case _:
             raise RuntimeError(f"Linux on {machine} is not supported")
 
@@ -32,10 +35,11 @@ def _bsd_candidates(system: str, processor: str) -> tuple[Target, ...]:
 
 
 class Target(enum.Enum):
-    X86_64_INTEL = enum.auto()
-    X86_64_ATT = enum.auto()
+    ARM32 = enum.auto()
     ARM64 = enum.auto()
     RISCV64 = enum.auto()
+    X86_64_ATT = enum.auto()
+    X86_64_INTEL = enum.auto()
 
     @staticmethod
     def candidates() -> tuple[Target, ...]:
@@ -55,11 +59,16 @@ class Target(enum.Enum):
 
     def intermediate_to_asm(self, intermediate: AST) -> Iterator[str]:
         match self:
-            case Target.X86_64_INTEL:
-                yield from generate_x86_64_intel(intermediate)
-            case Target.X86_64_ATT:
-                yield from generate_x86_64_att(intermediate)
+            case Target.ARM32:
+                yield from generate_arm32(intermediate)
             case Target.ARM64:
                 yield from generate_arm64(intermediate)
             case Target.RISCV64:
                 yield from generate_riscv64(intermediate)
+            case Target.X86_64_ATT:
+                yield from generate_x86_64_att(intermediate)
+            case Target.X86_64_INTEL:
+                yield from generate_x86_64_intel(intermediate)
+            case _:
+                raise RuntimeError(f"unhandled target {self}, this is a bug")
+
