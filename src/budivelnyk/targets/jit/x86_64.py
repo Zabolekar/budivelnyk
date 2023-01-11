fc8f09e507a129212583758665843c7b00ad2b23
Zabolekar <syekirin@gmail.com>
2023-01-11 01:11:57 +0100

    add more commands

--- src/budivelnyk/targets/jit/x86_64.py
+++ src/budivelnyk/targets/jit/x86_64.py
@@ -15,21 +15,21 @@ def _generate_x86_64(intermediate: AST) -> Iterator[bytes]:
     for node in intermediate:
         match node:
             case Add(1):
-                yield b"\xfe\x07"
+                yield b"\xfe\x07"  # inc byte ptr [rdi]
             case Add(n):
-                yield b"\x80\x07" + bytes([n])
+                yield b"\x80\x07" + bytes([n])  # add byte ptr [rdi], n
             case Subtract(1):
-                pass # TODO
+                yield b"\xfe\x0f"  # dec byte ptr [rdi]
             case Subtract(n):
-                pass # TODO
+                yield b"\x80\x2f" + bytes([n])  # sub byte ptr [rdi], n
             case Forward(1):
-                yield b"\x48\xff\xc7"
+                yield b"\x48\xff\xc7"  # inc rdi
             case Forward(n):
-                pass # TODO
+                yield b"\x48\x83\xc7" + bytes([n])  # add rdi, n
             case Back(1):
-                pass # TODO
+                yield b"\x48\xff\xcf"  # dec rdi
             case Back(n):
-                pass # TODO
+                yield b"\x48\x83\xef" + bytes([n])  # sub rdi, n
             case Output(n):
                 pass # TODO
             case Input(n):
