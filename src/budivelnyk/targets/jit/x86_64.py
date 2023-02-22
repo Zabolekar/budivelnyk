c51d51a66542712e95e90bbda97ab75a7d6141be
Zabolekar <syekirin@gmail.com>
2023-02-22 01:38:42 +0100

    implement Output(n) and Input(n) for JIT

--- src/budivelnyk/targets/jit/x86_64.py
+++ src/budivelnyk/targets/jit/x86_64.py
@@ -9,11 +9,19 @@ from .io import encoded_read_char, encoded_write_char
 
 
 def generate_x86_64(intermediate: AST) -> bytes:
-    return (b"".join(_generate_x86_64(intermediate)) + 
-            b"\xc3") # ret # TODO: make it epilogue
+    return (_generate_prologue()
+          + b"".join(_generate_body(intermediate))
+          + _generate_epilogue())
 
 
-def _generate_x86_64(intermediate: AST) -> Iterator[bytes]:
+def _generate_prologue():
+    return (b"\x41\x54"                       # push r12
+          + b"\x41\x55"                       # push r13
+          + b"\x49\xbc" + encoded_write_char  # movabs r12, encoded_write_char
+          + b"\x49\xbd" + encoded_read_char)  # movabs r13, encoded_read_char
+
+
+def _generate_body(intermediate: AST) -> Iterator[bytes]:
     for node in intermediate:
         match node:
             case Add(1):
@@ -32,13 +40,30 @@ def _generate_x86_64(intermediate: AST) -> Iterator[bytes]:
                 yield b"\x48\xff\xcf"  # dec rdi
             case Back(n):
                 yield b"\x48\x83\xef" + bytes([n])  # sub rdi, n
-            case Output(n): # TODO: wait, this is not!!!! n, this is 1
-                yield b"\x57"                 # push rdi
-                yield b"\x48\x0f\xb6\x3f"     # movzx rdi, byte ptr [rdi]
-                yield b"\x48\xbe" + encoded_write_char # movabs rsi, encoded_write_char TODO actually I should move it to some register in the prologue, same with encoded_read_char, and restore them in the epilogue
-                yield b"\xff\xd6"             # call rsi
-                yield b"\x5f"                 # pop rdi
+            case Output(n):
+                yield b"\x57"               # push rdi
+                yield b"\x48\x0f\xb6\x3f"   # movzx rdi, byte ptr [rdi]
+                sequence = [
+                    b"\x41\xff\xd4",        # call r12 (see prologue)
+                    b"\x48\x89\xc7"         # mov rdi, rax
+                ] * n
+                yield from sequence[:-1]
+                yield b"\x5f"               # pop rdi
             case Input(n):
-                pass # TODO
+                yield b"\x57"               # push   rdi
+                yield from [
+                    b"\x41\xff\xd5"         # call r13 (see prologue)
+                ] * n
+                yield b"\x5f"               # pop rdi
+                yield b"\x31\xd2"           # xor edx, edx
+                yield b"\x85\xc0"           # test eax, eax
+                yield b"\x0f\x48\xc2"       # cmovs eax, edx
+                yield b"\x88\x07"           # mov byte ptr [rdi], al
             case Loop(body):
                 pass # TODO
+
+
+def _generate_epilogue():
+    return (b"\x41\x5d"  # pop r13
+          + b"\x41\x5c"  # pop r12
+          + b"\xc3")     # ret
