a591fcda872d14de67a79dce2ed97eb572a9c8df
Zabolekar <syekirin@gmail.com>
2024-07-27 18:49:11 +0200

    expand tape tests

--- tests/test_tape.py
+++ tests/test_tape.py
@@ -4,25 +4,31 @@ import budivelnyk as bd
 
 
 def test_cast_bytes():
-    x = b"\x00"
+    x = b"\x12\x34\x56"
     with pytest.raises(TypeError, match="not writable"):
         bd.as_tape(x, size=1)
 
 
 def test_cast_bytearray():
-    x = bytearray(b"\x00")
-    bd.as_tape(x, size=1)
+    x = bytearray(b"\x12\x34\x56")
+    tape = bd.as_tape(x, size=1)
+    assert tape[:] == [0x12]
 
 
 def test_cast_int_array():
-    x = array('i', [1,2,3])
-    assert len(bd.as_tape(x)) == 3
-    assert len(bd.as_tape(x, 6)) == 6
+    x = array('i', [12,34,56])
+    accidentally_wrong_tape = bd.as_tape(x)
+    intentionally_wrong_tape = bd.as_tape(x, 6)
+    correct_tape = bd.as_tape(x, 12)
+    assert accidentally_wrong_tape[:] == [12, 0, 0]
+    assert intentionally_wrong_tape[:] == [12, 0, 0, 0, 34, 0]
+    assert correct_tape[:] == [12, 0, 0, 0, 34, 0, 0, 0, 56, 0, 0, 0]
 
 
 def test_copy_bytes():
-    x = b"\x00"
-    bd.make_tape(x)
+    x = b"\x12\x34\x56"
+    tape = bd.make_tape(x)
+    assert tape[:] == [0x12, 0x34, 0x56]
 
 
 def test_copy_bytearray():
