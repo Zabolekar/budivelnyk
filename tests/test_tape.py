e8327cc753626b8150fe168a650b504b71eb5917
Zabolekar <syekirin@gmail.com>
2025-05-30 16:12:09 +0200

    fix tests on big-endian hardware

--- tests/test_tape.py
+++ tests/test_tape.py
@@ -1,4 +1,6 @@
 from array import array
+import sys
+import platform
 import pytest
 import budivelnyk as bd
 
@@ -15,12 +17,24 @@ def test_cast_bytearray():
     assert tape[:] == [0x12]
 
 
-def test_cast_int_array():
+@pytest.mark.skipif(platform.machine() == "Power Macintosh", reason="most common hardware is little-endian")
+def test_cast_int_array_little_endian():
     x = array('i', [12, 34, 56])
     intentionally_wrong_tape = bd.as_tape(x, len(x))
     correct_tape = bd.as_tape(x, len(x) * x.itemsize)
+    assert sys.byteorder == "little"
     assert intentionally_wrong_tape[:] == [12, 0, 0]
     assert correct_tape[:] == [12, 0, 0, 0, 34, 0, 0, 0, 56, 0, 0, 0]
+
+
+@pytest.mark.skipif(platform.machine() != "Power Macintosh", reason="PowerPC Macs are big-endian")
+def test_cast_int_array_big_endian():
+    x = array('i', [12, 34, 56])
+    intentionally_wrong_tape = bd.as_tape(x, len(x))
+    correct_tape = bd.as_tape(x, len(x) * x.itemsize)
+    assert sys.byteorder == "big"
+    assert intentionally_wrong_tape[:] == [0, 0, 0]
+    assert correct_tape[:] == [0, 0, 0, 12, 0, 0, 0, 34, 0, 0, 0, 56]
 
 
 def test_copy_bytes():
