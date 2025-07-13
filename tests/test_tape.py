6a3ddbf1b07749ab621ea65acbed342d43b82db2
Zabolekar <syekirin@gmail.com>
2025-07-13 17:30:22 +0200

    conditional asserts instead of skipping tests

--- tests/test_tape.py
+++ tests/test_tape.py
@@ -17,24 +17,18 @@ def test_cast_bytearray():
     assert tape[:] == [0x12]
 
 
-@pytest.mark.skipif(platform.machine() == "Power Macintosh", reason="most common hardware is little-endian")
-def test_cast_int_array_little_endian():
+def test_cast_int_array():
     x = array('i', [12, 34, 56])
     intentionally_wrong_tape = bd.as_tape(x, len(x))
     correct_tape = bd.as_tape(x, len(x) * x.itemsize)
-    assert sys.byteorder == "little"
-    assert intentionally_wrong_tape[:] == [12, 0, 0]
-    assert correct_tape[:] == [12, 0, 0, 0, 34, 0, 0, 0, 56, 0, 0, 0]
-
-
-@pytest.mark.skipif(platform.machine() != "Power Macintosh", reason="PowerPC Macs are big-endian")
-def test_cast_int_array_big_endian():
-    x = array('i', [12, 34, 56])
-    intentionally_wrong_tape = bd.as_tape(x, len(x))
-    correct_tape = bd.as_tape(x, len(x) * x.itemsize)
-    assert sys.byteorder == "big"
-    assert intentionally_wrong_tape[:] == [0, 0, 0]
-    assert correct_tape[:] == [0, 0, 0, 12, 0, 0, 0, 34, 0, 0, 0, 56]
+    if platform.machine() == "Power Macintosh":
+        assert sys.byteorder == "big"
+        assert intentionally_wrong_tape[:] == [0, 0, 0]
+        assert correct_tape[:] == [0, 0, 0, 12, 0, 0, 0, 34, 0, 0, 0, 56]
+    else:
+        assert sys.byteorder == "little"
+        assert intentionally_wrong_tape[:] == [12, 0, 0]
+        assert correct_tape[:] == [12, 0, 0, 0, 34, 0, 0, 0, 56, 0, 0, 0]
 
 
 def test_copy_bytes():
